e5eb8a602fba1d35818226f60ca104f925a27b09
Damon Toumbourou <dtoumbourou@gmail.com>
2016-01-24 19:43:56 +1100

    finished GUI

--- addon.py
+++ addon.py
@@ -1,7 +1,6 @@
 from xbmcswift2 import Plugin, xbmcgui
 from resources.lib import cook
 import pyxbmct.addonwindow as pyxbmct
-import os
 
 plugin = Plugin()
 
@@ -29,8 +28,8 @@ def search(page_num, search_keyword=""):
     result = cook.get_search(page_num, search_keyword)
     
     next_page = 1
-    if not page_num:
-        next_page = int(page_num) + 1
+    #if not page_num:
+    next_page = int(page_num) + 1
 
     item = []
     for i in result:
@@ -44,17 +43,18 @@ def search(page_num, search_keyword=""):
         'label': 'NEXT PAGE',
         'path': plugin.url_for('search', page_num = next_page, search_keyword=search_keyword),
     })
-
     
     return item
 
 
 @plugin.route('/recipe/<url>/')
 def get_recipe(url):
-    METHOD_NAME = 'Method'
+    METHOD_NAME = 'METHOD:'
+    INGRED_NAME = 'INGREDIENTS: '
 
     item = cook.get_recipe(url)
-
+    
+    # Fetch recipe info from website with scraper
     for i in item:
         title = i['title']
         sub_title = i['sub_title']
@@ -67,20 +67,32 @@ def get_recipe(url):
         ing = i['ing']
         diff = i['diff']
         serv = i['serv']
-        image= i['img']
-
+        image = i['img']
+        star = i['star']
+        source = i['source']
+    
+    # Fetch method steps from website with scraper
     method_steps = []
     method = cook.get_recipe_method(url)
     for i in method:
         method_steps.append({
             'step': i['step'],
-    
     })
-
+    
+    # Fetch ingredients from website with scraper
+    ingred_list = [] 
+    ingred = cook.get_recipe_ingred(url)
+    for i in ingred:
+        ingred_list.append({
+            'ingred': i['ing'],
+    })
+    
+    # Run GUI classes
     window = RecipeWindow(title)
     window.set_sub_title(sub_title)
-    window.set_recipe_info(prep_time, cook_time, ing, diff, serv)
-    #window.set_image(label, image)
+    window.set_recipe_info(prep_time, cook_time, ing, diff, serv, star, source)
+    window.set_image(image)
+    window.set_ingred(INGRED_NAME, ingred_list)
     window.set_method(METHOD_NAME, method_steps)
     window.doModal()
     del window
@@ -90,86 +102,125 @@ class RecipeWindow(pyxbmct.AddonDialogWindow):
 
     def __init__(self, title=''):
         super(RecipeWindow, self).__init__(title)
-        self.setGeometry(1050, 650, 9, 4)
+        self.setGeometry(1280, 720, 9, 4)
         # connenct a key action (Backspace) to close window.
         self.connect(pyxbmct.ACTION_NAV_BACK, self.close)
 
     def set_sub_title(self, sub_title):
         # recipe sub title
-        self.textbox = pyxbmct.TextBox()
-        self.placeControl(self.textbox, 0, 0, 2, 4)
-        self.textbox.setText(sub_title)
+        label = pyxbmct.Label(sub_title, alignment=pyxbmct.ALIGN_JUSTIFY)
+        self.placeControl(label, 0, 0, columnspan = 4)
 
-    def set_recipe_info(self, prep_time, cook_time, ing, diff, serv):
-        self.label = pyxbmct.Label('Prep Time: \n' + prep_time)
-        self.placeControl(self.label, 1, 0)
+    def set_recipe_info(self, prep_time, cook_time, ing, diff, serv, star, source):
+        # Prep time label
+        self.label = pyxbmct.Label('Prep Time: ')
+        self.placeControl(self.label, 0.7, 3)
         #
-        self.label = pyxbmct.Label('Cook Time: \n' + cook_time)
-        self.placeControl(self.label, 1, 0.6)
+        self.label = pyxbmct.Label(prep_time, textColor='0xff808080')
+        self.placeControl(self.label, 0.7, 3.4) 
+        # Cook time label
+        self.label = pyxbmct.Label('Cook Time: ')
+        self.placeControl(self.label, 1.1, 3)
         #
-        self.label = pyxbmct.Label('Ingredients: \n' + ing)
-        self.placeControl(self.label, 1, 1.2)   
+        self.label = pyxbmct.Label(cook_time, textColor='0xff808080')
+        self.placeControl(self.label, 1.1, 3.4)
+        # Ingred label
+        self.label = pyxbmct.Label('Ingredients: ')
+        self.placeControl(self.label, 1.5, 3)   
+        # 
+        self.label = pyxbmct.Label(ing, textColor='0xff808080')
+        self.placeControl(self.label, 1.5, 3.4)
+        # Diff label
+        self.label = pyxbmct.Label('Difficulty: ')
+        self.placeControl(self.label, 1.9, 3)
         #
-        self.label = pyxbmct.Label('Difficulty: \n' + diff)
-        self.placeControl(self.label, 1, 1.8)
+        self.label = pyxbmct.Label(diff, textColor='0xff808080')
+        self.placeControl(self.label, 1.9, 3.4)
+        # Serve label
+        self.label = pyxbmct.Label('Serves: ')
+        self.placeControl(self.label, 2.3, 3)
         #
-        self.label = pyxbmct.Label('Serves: \n' + serv)
-        self.placeControl(self.label, 1, 2.4)
-    
+        self.label = pyxbmct.Label(serv, textColor='0xff808080')
+        self.placeControl(self.label, 2.3, 3.4)
+        # Star Rating label 
+        self.label = pyxbmct.Label('Stars: ')
+        self.placeControl(self.label, 2.7, 3)
+        # 
+        self.label = pyxbmct.Label(star, textColor='0xff808080')
+        self.placeControl(self.label, 2.7, 3.4)
+        # Source label
+        self.label = pyxbmct.Label('Source: ')
+        self.placeControl(self.label, 3.1, 3)
+        # 
+        self.label = pyxbmct.Label(source, textColor='0xff808080')
+        self.placeControl(self.label, 3.1, 3.4)
+ 
     def set_method(self, METHOD_NAME, method_steps):
         # List
+        list_label = pyxbmct.Label(METHOD_NAME)
+        self.placeControl(list_label, 4, 1.65)
+        #
+        self.list_item_label = pyxbmct.TextBox()
+        self.placeControl(self.list_item_label, 4.5, 1.9, 4, 2)
+        # List
         self.list = pyxbmct.List()
-        self.placeControl(self.list, 3, 3, 3, 1)
+        self.placeControl(self.list, 4.5, 1.5, 5, 0.3)
         # Add items to the list
+        step_num = 1
         for i in method_steps:
-            items = ['Step: ' + i['step'] ]
+            items = ['{0}.  '.format(step_num) + i['step']] 
             self.list.addItems(items)
-            # Connect the list to a function to display which list is selected
-            self.connect(self.list, lambda: xbmc.executebuiltin('Notification(Note!,{0} selected.)'.format(
-                self.list.getListItem(self.list.getSelectedPosition()).getLabel())))
-            # Connect key and mouse events for the list navaigation feedback
-            self.connectEventList(
-                    [pyxbmct.ACTION_MOVE_DOWN,
-                     pyxbmct.ACTION_MOVE_UP,
-                     pyxbmct.ACTION_MOUSE_WHEEL_DOWN,
-                     pyxbmct.ACTION_MOUSE_WHEEL_UP],
-                     self.list_update)
-
+            step_num = step_num + 1
+            
+        # Connect the list to a function to display which list is selected
+        self.connect(self.list, lambda: xbmc.executebuiltin('Notification(Note!,{0} selected.)'.format(
+            self.list.getListItem(self.list.getSelectedPosition()).getLabel())))
+        # Connect key and mouse events for the list navaigation feedback
+        self.connectEventList(
+            [pyxbmct.ACTION_MOVE_DOWN,
+             pyxbmct.ACTION_MOVE_UP,
+             pyxbmct.ACTION_MOUSE_WHEEL_DOWN,
+             pyxbmct.ACTION_MOUSE_WHEEL_UP,
+             pyxbmct.ACTION_MOUSE_MOVE],  
+             self.list_update)
+    
+    def set_ingred(self, INGRED_NAME, ingred_list):
+        # Label for ingredients
+        self.label = pyxbmct.Label(INGRED_NAME)
+        self.placeControl(self.label, .6, 0)
+        # List of ingredients
+        self.list = pyxbmct.List()
+        self.placeControl(self.list, 1, 0, 8, 2)
+        # Add items to the list
+        step_num = 1
+        for i in ingred_list:
+            items = ['- ' + i['ingred'] ]
+            self.list.addItems(items)
+            step_num = step_num + 1
+    
     def list_update(self):
         try:
             if self.getFocus() == self.list:
-                self.list_item_label.setLabel(self.list.getListItem(self.list.getSelectedPosition().getLabel())
+                self.list_item_label.setText(self.list.getListItem(self.list.getSelectedPosition()).getLabel())
             else:
-                self.list_item_label.setLabel`
+                self.list_item_label.setText('')
+        except (RuntimeError, SystemError):
+            pass
 
-    """
-    def set_method(self, METHOD_NAME, method_steps):
-        list_label = pyxbmct.Label(METHOD_NAME)
-        self.placeControl(list_label, 2.5, 2, 4, 4)
-        # add steps to the list
-        self.list = pyxbmct.List()
-        self.placeControl(self.list, 3, 0, 2, 2)
-        
-        vert_place = 3
-        step_num = 1
-        for i in method_steps:
-            #items = ['Step {0}: '.format(step_num) + i['step'] ]
-            #step_num = step_num + 1 
-            #self.list.addItems(items)
-            self.textbox = pyxbmct.TextBox()
-            self.placeControl(self.textbox, vert_place, 0, 2, 4)
-            self.textbox.setText('Step {0}: '.format(step_num) + i['step'])
-            vert_place = vert_place + 2
-            step_num = step_num + 1
-    """
-    def set_image(self, label, img):
-        # image label
+    def set_image(self, img):
+        # img
         self.img = img
-        label = pyxbmct.Label(label)
-        self.placeControl(label, 5, 0)
-        #image
         self.image = pyxbmct.Image(self.img)
-        self.placeControl(self.image, 6, 1, 3, 2)
+        self.placeControl(self.image, 0.8, 1.95, 3, 1)
+
+# For side window recipe info when browsing recipes
+class SideWindow(pyxbmct.AddonDialogWindow):
+
+    def __init__(self, title=''):
+        super(SideWindow, self).__init__(title)
+        self.setGeometry(200, 300, 5, 2)
+        
+
 
 if __name__ == '__main__':
     plugin.run()
--- resources/lib/cook.py
+++ resources/lib/cook.py
@@ -3,32 +3,29 @@ import urllib2
 import requests 
 import re
 
-"""
-def get_category(url):
-    soup = get_soup(url)
-    content = soup.find_all('div', {'class': 'popular-collection clearfix'})
-    content = soup.find_all('h2')
+def get_soup(url):
+    html = urllib2.urlopen(url).read()
+    html = html.replace("</scr' + 'ipt>","")
+    soup = bs(html, 'html.parser')
 
-    for i in content:
-        print i
-#get_category('http://www.taste.com.au/')
-"""
+    return soup
 
 def get_search(page_num, keyword):
     keyword = keyword.replace(' ', '+')
     SEARCH_URL = 'http://www.taste.com.au/search-recipes/?q=' + keyword + '&page=' + str(page_num) 
     
-    html = urllib2.urlopen(SEARCH_URL).read()
-    html = html.replace("</scr' + 'ipt>","")
-    soup = bs(html, 'html.parser')
-    content = soup.find('div', {'class': 'content-item'})
-    content = soup.find_all('div', {'class': 'story-block '})
+    soup = get_soup(SEARCH_URL)
+   
+    content = soup.find('div', {'class': 'content-item tab-content current'})
+    content = content.find_all('div', {'class': 'story-block '})
     
     output = []
     for i in content:
         path = i.find('a').get('href')
         
         label = i.get_text().strip()
+        print 'label: '
+        print label
         try:
             thumb = i.find('img')['src']
             thumb =  thumb.replace('_s', '_l')
@@ -46,45 +43,67 @@ def get_search(page_num, keyword):
         output.append(item)
 
     return output
-#iget_search(1,'cheese sticks')
-
+#get_search(1,'mint')
 
 def get_recipe(url):
-    html = urllib2.urlopen(url).read()
-    html = html.replace("</scr' + 'ipt>","")
-    soup = bs(html, 'html.parser')
+    ing = ''
+    diff = ''
+    star = ''
+    source = ''
+
+    soup = get_soup(url)
 
     title = soup.find('div', {'class': 'heading'})
     title = title.get_text().strip() 
     
     sub_title = soup.find('p', {'class': 'quote-left'})
     sub_title = sub_title.get_text()
-
-    prep_time = soup.find('td', {'class': 'prepTime'})
-    prep_time = prep_time.find('em').get_text()
-
-    cook_time = soup.find('td', {'class': 'cookTime'})
-    cook_time = cook_time.find('em').get_text()
-
-    ing = soup.find('td', {'class': 'ingredientCount'})
-    ing = ing.find('em').get_text()
     
-    diff = soup.find('td', {'class': 'difficultyTitle'})
-    diff = diff.find('em').get_text()
+    try: 
+        prep_time = soup.find('td', {'class': 'prepTime'})
+        prep_time = prep_time.find('em').get_text()
+    except AttributeError:
+        pass
+
+    try:
+        cook_time = soup.find('td', {'class': 'cookTime'})
+        cook_time = cook_time.find('em').get_text()
+    except AttributeError:
+        pass
+    
+    try:
+        ing = soup.find('td', {'class': 'ingredientCount'})
+        ing = ing.find('em').get_text()
+    except AttributeError:
+        pass
+    
+    try:
+        diff = soup.find('td', {'class': 'difficultyTitle'})
+        diff = diff.find('em').get_text()
+    except AttributeError:
+        pass
+
+    try:
+        star = soup.find('span', {'class': 'star-level'})
+        star = star.get_text()
+    except AttributeError:
+        pass
+
+    try:
+        source = soup.find('div', {'class': 'source-logo'})
+        source = source.get_text().strip()
+    except AttributeError:
+        pass
     
     try:
         serv = soup.find('td', {'class': 'servings'})
         serv = serv.find('em').get_text()
-        print serv
     except AttributeError:
         pass
 
     if not serv:
         serv = soup.find('td', {'class': 'makes'})
         serv = serv.find('em').get_text()
-        print serv
-
-    print serv
 
     img = soup.find('img', {'itemprop': 'photo'})
     img = img.get('src')
@@ -99,18 +118,18 @@ def get_recipe(url):
             'diff': diff,
             'serv': serv,
             'img': img,
+            'star': star,
+            'source': source,
     }
 
     output.append(items)
     
     return output
-#get_recipe('http://www.taste.com.au/recipes/29331/hazelnut+milk+chocolates')
-
+get_recipe('http://www.taste.com.au/recipes/29331/hazelnut+milk+chocolates')
 
 def get_recipe_method(url):
-    html = urllib2.urlopen(url).read()
-    html = html.replace("</scr' + 'ipt>","")
-    soup = bs(html, 'html.parser')
+    soup = get_soup(url)
+
     step = soup.find_all('p', {'class': 'description'})
 
     output = []
@@ -124,3 +143,20 @@ def get_recipe_method(url):
 
     return output
 #get_recipe_method('http://www.taste.com.au/recipes/19197/warm+milk+with+a+twist')
+
+def get_recipe_ingred(url):
+    soup = get_soup(url)
+
+    content = soup.find_all('ul', {'class': 'ingredient-table'})
+    ing_list = [] 
+    for i in content:
+        con = i.find_all('li') 
+        for k in con:
+            ing = k.get_text().strip()
+            item = {
+                'ing': ing,
+            }
+            ing_list.append(item)
+
+    return ing_list
+#get_recipe_ingred('http://www.taste.com.au/recipes/3213/chocolate+mint+cakes+with+choc+mint+frosting')
